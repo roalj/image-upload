77be3a443423e2efaf44f3daf5f9e8ae0654ee33
pastafariangit <aljosa.omejc@gmail.com>
2019-11-19 22:07:26 +0100

    added test mongo connection

--- api/src/main/java/resource/ImageResource.java
+++ api/src/main/java/resource/ImageResource.java
@@ -1,6 +1,9 @@
 package resource;
 
 import beans.ImageBean;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
 import entities.ImageEntity;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -22,6 +25,7 @@ public class ImageResource {
     @GET
     public Response getImageList() {
         final List<entities.ImageEntity> imageList = imageBean.getImageList();
+        connectToMongo();
         return Response.ok(imageList).build();
     }
 
@@ -30,11 +34,25 @@ public class ImageResource {
     public Response getImageMetadata(@PathParam("imageId") Integer imageId) {
 
         ImageEntity image = imageBean.getImage(imageId);
-
         if (image == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
         return Response.status(Response.Status.OK).entity(image).build();
+    }
+
+    private void connectToMongo(){
+        try {
+            MongoClientURI uri = new MongoClientURI(
+                    "mongodb+srv://pastafarian:goldi1@cluster0-mtykz.gcp.mongodb.net/test?retryWrites=true&w=majority");
+            MongoClient mongoClient = new MongoClient(uri);
+            MongoDatabase database = mongoClient.getDatabase("test");
+            database.createCollection("hello aljosa");
+            System.out.println("created collection");
+
+        }catch (Exception e){
+            System.out.println("exception: " + e.toString());
+            e.printStackTrace();
+        }
     }
 }
